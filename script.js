ffa41371f80dbabc641a869698760961bc167e8c
crystal00lim <crystalbb00@gmail.com>
2020-11-05 20:30:50 +0000

    edited javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+  function lovelythings(sw) {
+    var pic;
+    if (sw==0) {
+      pic = "P1060700.JPG"
+    } else {
+      pic = "P1060578.JPG"
+    }
+    document.getElementById('myImage').src = pic;
+  }
