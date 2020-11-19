a802ff213cd56a0a9503175744913474f025b491
crystal00lim <crystalbb00@gmail.com>
2020-11-19 21:19:44 +0000

    makeover

--- script.js
+++
@@ -1,9 +0,0 @@
-  function lovelythings(sw) {
-    var pic;
-    if (sw==0) {
-      pic = "P1060700.JPG"
-    } else {
-      pic = "P1060578.JPG"
-    }
-    document.getElementById('myImage').src = pic;
-  }
